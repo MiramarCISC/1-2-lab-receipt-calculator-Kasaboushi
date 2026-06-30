89fd9ff7cb314de3c9a0aac3e2a6e30f93a9af34
Kasaboushi <68836524+Kasaboushi@users.noreply.github.com>
2026-06-30 05:51:05 +0000

    assignment 1; implemented functions to receipt.cpp

--- src/receipt.cpp
+++ src/receipt.cpp
@@ -1,23 +1,30 @@
 #include "receipt.hpp"
+#include <iostream>
+using namespace std;
 
 // TODO: Return the sum of all three item prices.
 double calculateSubtotal(double item1, double item2, double item3) {
-    return 0.0;
+    double sub = item1 + item2 + item3;
+    return sub;
 }
 
 // TODO: Return the sales tax for the subtotal using TAX_RATE.
 double calculateTax(double subtotal) {
-    return 0.0;
+    double tax = subtotal*TAX_RATE;
+    return tax;
 }
 
 // TODO: Return the tip amount.
 // Example: if subtotal is 20.00 and tipPercent is 15,
 // the tip should be 3.00.
 double calculateTip(double subtotal, double tipPercent) {
-    return 0.0;
+    double tipRate = tipPercent/100;
+    double tip = subtotal*tipRate;
+    return tip;
 }
 
 // TODO: Return the final total.
 double calculateTotal(double subtotal, double tax, double tip) {
-    return 0.0;
+    double total = subtotal + tax + tip;
+    return total;
 }
