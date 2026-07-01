60a8056a1d835b16a88c60e6c893b1c351d640dc
Kasaboushi <68836524+Kasaboushi@users.noreply.github.com>
2026-07-01 17:40:33 +0000

    Removed Instructions

--- src/receipt.cpp
+++ src/receipt.cpp
@@ -2,28 +2,22 @@
 #include <iostream>
 using namespace std;
 
-// TODO: Return the sum of all three item prices.
 double calculateSubtotal(double item1, double item2, double item3) {
     double sub = item1 + item2 + item3;
     return sub;
 }
 
-// TODO: Return the sales tax for the subtotal using TAX_RATE.
 double calculateTax(double subtotal) {
     double tax = subtotal*TAX_RATE;
     return tax;
 }
 
-// TODO: Return the tip amount.
-// Example: if subtotal is 20.00 and tipPercent is 15,
-// the tip should be 3.00.
 double calculateTip(double subtotal, double tipPercent) {
     double tipRate = tipPercent/100;
     double tip = subtotal*tipRate;
     return tip;
 }
 
-// TODO: Return the final total.
 double calculateTotal(double subtotal, double tax, double tip) {
     double total = subtotal + tax + tip;
     return total;
